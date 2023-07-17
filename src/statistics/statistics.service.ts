33cb9745a66371bf79c457e8537d11479f964d32
osappy <904929+osappy@users.noreply.github.com>
2023-07-17 11:27:01 +0900

    update searchlog

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -7,12 +7,9 @@ export class StatisticsService {
   constructor(
     @Inject(MONGODB_SEARCH_STATISTICS) private readonly ssService: Collection,
   ) {}
-  async sendSearchKeyword(searchResultMeta: any) {
-    delete searchResultMeta.latitude;
-    delete searchResultMeta.longitude;
-    this.ssService.insertOne({
-      ...searchResultMeta,
-      createdAt: new Date().toISOString(),
-    });
+  async sendSearchLog(searchLog: any) {
+    delete searchLog.latitude;
+    delete searchLog.longitude;
+    this.ssService.insertOne(searchLog);
   }
 }
