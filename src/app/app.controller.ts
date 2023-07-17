1da14adec98514cdf32b0f745bd793cbccf90a1f
osappy <904929+osappy@users.noreply.github.com>
2023-07-17 10:27:34 +0900

    add current-location

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -11,6 +11,7 @@ import {
 import { GoogleGeocodingService } from '../google-geocoding/google-geocoding.service';
 import { MedicalinstitutionService } from 'src/medicalinstitution/medicalinstitution.service';
 import { SearchInterceptor } from 'src/intercepter/search/search.interceptor';
+import { PharmacyService } from 'src/pharmacy/pharmacy.service';
 
 @UseInterceptors(SearchInterceptor)
 @Controller()
@@ -18,6 +19,7 @@ export class AppController {
   constructor(
     private readonly googleGeocodeService: GoogleGeocodingService,
     private readonly miService: MedicalinstitutionService,
+    private readonly phaService: PharmacyService,
   ) {}
 
   @Get('/api/v1/search/current-location/medical-institutions')
@@ -153,6 +155,85 @@ export class AppController {
             }`
           : null,
       last: `/api/v1/search/current-location/medical-institutions?todofuken=${todofuken}&shikuchoson=${shikuchoson}&page=${meta.totalPages}`,
+    };
+    const results = records
+      .slice(
+        (page - 1) * itemsPerPage,
+        (page - 1) * itemsPerPage + itemsPerPage,
+      )
+      .map((item) => {
+        return {
+          name: item.name,
+          postalcode: item.postalcode,
+          address: item.address,
+          tel: item.phone,
+          url: item.website,
+          memo_available_time: item.available_date,
+          location: {
+            lat: item.location.coordinates[1],
+            lng: item.location.coordinates[0],
+          },
+        };
+      });
+
+    return { meta, links, results };
+  }
+
+  @Get('/api/v1/search/current-location/pharmacies')
+  async searchPharmacy_currentlocation(
+    @Query('latitude', ParseFloatPipe) latitude: number,
+    @Query('longitude', ParseFloatPipe) longitude: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('perpage', new DefaultValuePipe(20), ParseIntPipe)
+    itemsPerPage: number,
+  ) {
+    const { todofuken, shikuchoson } =
+      await this.googleGeocodeService.getAddress(latitude, longitude);
+
+    const records = await this.phaService.searchFromCurrentLocation(
+      latitude,
+      longitude,
+    );
+
+    const totalItems = records.length;
+
+    // pageパラメータが不正な場合は、1ページ目を返す。 TODO: Pipeで実装する
+    if (page < 1 || page > Math.ceil(totalItems / itemsPerPage)) {
+      page = 1;
+    }
+
+    // perpageパラメータが不正な場合は、20件を返す。 TODO: Pipeで実装する
+    if (itemsPerPage < 1 || itemsPerPage > 100) {
+      itemsPerPage = 20;
+    }
+
+    const meta = {
+      itemsPerPage,
+      totalItems,
+      currentPage: page,
+      totalPages: Math.ceil(totalItems / itemsPerPage),
+      searchType: 'current-location',
+      latitude: latitude,
+      longitude: longitude,
+      address_todofuken: todofuken,
+      address_shikuchoson: shikuchoson,
+    };
+    const links = {
+      current: `/api/v1/search/current-location/pharmacy?latitude=${latitude}&longitude=${longitude}&page=${page}`,
+      first: `/api/v1/search/current-location/pharmacy?latitude=${latitude}&longitude=${longitude}&page=1`,
+      prev:
+        page > 1
+          ? `/api/v1/search/current-location/pharmacy?latitude=${latitude}&longitude=${longitude}&page=${
+              page - 1
+            }`
+          : null,
+      next:
+        page < meta.totalPages
+          ? `/api/v1/search/current-location/pharmacy?latitude=${latitude}&longitude=${longitude}&page=${
+              page + 1
+            }`
+          : null,
+      last: `/api/v1/search/current-location/pharmacy?latitude=${latitude}&longitude=${longitude}&page=${meta.totalPages}`,
     };
     const results = records
       .slice(
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GoogleGeocodingModule } from '../google-geocoding/google-geocoding.module';
 import { MedicalinstitutionModule } from 'src/medicalinstitution/medicalinstitution.module';
 import { StatisticsModule } from 'src/statistics/statistics.module';
+import { PharmacyModule } from 'src/pharmacy/pharmacy.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { StatisticsModule } from 'src/statistics/statistics.module';
     }),
     GoogleGeocodingModule,
     MedicalinstitutionModule,
+    PharmacyModule,
     StatisticsModule,
   ],
   controllers: [AppController],
