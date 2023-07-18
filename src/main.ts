edb847b6bc855f6f97a3c1386e8664b0e8cacba3
osappy <904929+osappy@users.noreply.github.com>
2023-07-18 22:04:10 +0900

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT) || 3000;
+
+  const corsOrigin =
+    process.env.NODE_ENV === 'production' ? 'https://afterpill.jp' : '*';
+
+  app.enableCors({
+    origin: corsOrigin,
+    methods: 'GET',
+  });
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
